bd1090dc44f13d2aeeff12d7132120e8d244271f
arunveguru <arunveguru2005@gmail.com>
2026-04-04 23:55:02 +0530

    Updated controller.py to get the LLM generated questions to the user.

--- core/controller.py
+++ core/controller.py
@@ -1,4 +1,7 @@
 import streamlit as st
+from utils.parser import parse_tech_stack
+from llm.geminiAPI import call_gemini
+from llm.prompts import generate_questions_prompt
 
 EXIT_KEYWORDS = ["exit", "quit", "bye", "done"]
 
@@ -46,12 +49,25 @@ def handle_input(user_input):
         return "List your tech stack (languages, frameworks, tools)."
     
     elif state == "ask_tech_stack":
-        st.session_state.candidate["tech_stack"] = user_input
+        tech_list = parse_tech_stack(user_input)
+        st.session_state.candidate["tech_stack"] = tech_list
         st.session_state.state = "generate_questions"
-        return "Thanks! Generating technical questions for you..."
+        return f"Got it! Tech stack identified: {', '.join(tech_list)}.\n\nGenerating questions..."
     
     elif state == "generate_questions":
-        return "Questions will be generated in next step..."
+        candidate = st.session_state.candidate
+
+        prompt = generate_questions_prompt(
+            candidate["experience"],
+            candidate["tech_stack"]
+        )
+
+        questions = call_gemini(prompt)
+
+        st.session_state.questions = questions
+        st.session_state.state = "end"
+
+        return f"Here are your technical questions:\n\n{questions}\n\n Thank you for applying! We will get back to you soon."
     
     elif state == "end":
         return "Conversation already ended."
